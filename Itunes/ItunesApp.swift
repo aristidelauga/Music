06a68a2d1aa467095acefa42a314e09ed592688c
aristidelauga <aristide.lauga@gmail.com>
2023-05-30 18:06:47 +0200

    Initial commit

---
+++ Itunes/ItunesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItunesApp.swift
+//  Itunes
+//
+//  Created by Aristide LAUGA on 30/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct ItunesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
